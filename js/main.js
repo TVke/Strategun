284fd33b9eefd4a2fb719ecc3ed107e13eeb1804
Vincent De Coen <Vincentdc94@users.noreply.github.com>
2017-03-07 10:40:04 +0100

    loading characters boilerplate

--- js/main.js
+++ js/main.js
@@ -18,12 +18,23 @@ var gameObjects = {
 	tile: new Tile(game),
 }
 
+var characters = {
+	bomb: Bomb,
+	flag: Flag,
+	medic: Medic,
+	sniper: Sniper,
+	soldier: Soldier,
+	tank: Tank,
+}
+
 function preload() {
 	Object.keys(gameObjects).forEach(function (object) {
 		gameObjects[object].load();
 	});
 
-	
+	Object.keys(characters).forEach(function(character) {
+		characters[character].load();
+	})
 }
 
 function create() {
