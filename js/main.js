789ae42bc2d77621c90badbd4c9a49afdb548a6f
Thomas <thomas_verhelst@telenet.be>
2017-03-07 10:23:27 +0100

    fixed merge conflict

--- js/main.js
+++ js/main.js
@@ -7,11 +7,7 @@
 //const height = 0;
 //scaleRatio = window.devicePixelRatio / 3;
 
-<<<<<<< HEAD
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, '', {
-=======
-var game = new Phaser.Game(window.innerWidth, window.innerHeight / window.devicePixelRatio, Phaser.CANVAS, '', {
->>>>>>> 54ad50594d1e5fa2f6df2776af78ec5b76f3cf8a
 	preload: preload,
 	create: create,
 	update: update
