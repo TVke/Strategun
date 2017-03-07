bdb56f5ceff106a9d282fb4f1d8f302fa7fa9876
TVke <thomas.verhelst@student.kdg.be>
2017-03-07 14:48:00 +0100

    added scale stuff and beter sprites

--- js/world/scale.js
+++ js/world/scale.js
@@ -1,23 +1,27 @@
 function Scale(game){
   this.game = game;
-  this.zoomIn = true;
   this.buttonSpace = 20;
   this.assetSize = 55;
-  this.zoomButton;
 }
+var zoomButton,zoomIn = true;
 
 Scale.prototype.load = function(){
   this.game.load.image('scaleIn','assets/grid/loop plus.png');
   this.game.load.image('scaleOut','assets/grid/loop min.png');
 }
 Scale.prototype.makeButton = function(){
-    this.zoomButton = this.game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize),window.innerHeight - (this.buttonSpace + this.assetSize),'scaleOut');
-    this.zoomButton.fixedToCamera = true;
+    zoomButton = game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize),window.innerHeight - (this.buttonSpace + this.assetSize),'scaleOut',gameObjects.scale.changeZoomLevel);
+    zoomButton.fixedToCamera = true;
 }
 Scale.prototype.changeZoomLevel = function () {
-    if(zoomIn){
-        this.game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize), window.innerHeight - (this.buttonSpace + this.assetSize), 'scaleIn');
+    zoomButton.destroy();
+    if(this.zoomIn){
+        zoomButton = game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize), window.innerHeight - (this.buttonSpace + this.assetSize), 'scaleIn',gameObjects.scale.changeZoomLevel);
+        zoomButton.fixedToCamera = true;
+        this.zoomIn = false;
     }else{
-        this.game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize),window.innerHeight - (this.buttonSpace + this.assetSize),'scaleOut');
+        zoomButton = game.add.button(window.innerWidth - (this.buttonSpace + this.assetSize),window.innerHeight - (this.buttonSpace + this.assetSize),'scaleOut',gameObjects.scale.changeZoomLevel);
+        zoomButton.fixedToCamera = true;
+        this.zoomIn = true;
     }
 }
