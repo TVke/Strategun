3151faa5314056374e2cbcde024e77eec819519e
Sander Borret <sander.borret@student.kdg.be>
2017-03-07 11:54:49 +0100

    Balk toegevoegd

--- js/world/tiles.js
+++ js/world/tiles.js
@@ -5,18 +5,18 @@ function Tile(game){
 Tile.prototype.load = function(){
 	for (let i = 0; i < 25; ++i) {
 		for (let j = 0; j < 50; ++j) {
-			let graphics = game.add.graphics(0, 0);
-			graphics.beginFill(0xFFFFFF);
-			graphics.drawRect(tileSize * j, tileSize * i, tileSize, tileSize);
-			graphics.inputEnabled = true;
-			graphics.events.onInputOver.add(over, this);
-			graphics.events.onInputOut.add(out, this);
+			let invisibleTile = game.add.graphics(0, 0);
+			invisibleTile.beginFill(0xFFFFFF);
+			invisibleTile.drawRect(tileSize * j, tileSize * i, tileSize, tileSize);
+			invisibleTile.inputEnabled = true;
+			invisibleTile.events.onInputOver.add(over, this);
+			invisibleTile.events.onInputOut.add(out, this);
 		}
 	}
-}
 
-Tile.prototype.invisibleTiles = function() {
-
+    let tileSelectorBackground = game.add.graphics(0, 0);
+    tileSelectorBackground.beginFill(0x000000);
+    tileSelectorBackground.drawRect(0, 0, tileSize * 50, tileSize);
 }
 
 Tile.prototype.highlight = function(position, range){
