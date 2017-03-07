89e330ab3a06a7cc6fdc53cdb488f292ae3628d2
Vincent De Coen <Vincentdc94@users.noreply.github.com>
2017-03-07 16:28:31 +0100

    check movement possible in progress

--- js/characters/character.js
+++ js/characters/character.js
@@ -16,10 +16,28 @@ Character.prototype.move = function(){
     //TODO: Movement
 }
 
+Character.moveableLocation = function(x, y){
+
+}
+
 Character.events = function(){
 
     game.input.onTap.add(function(pointer, event){
-        neighbours = Coords.neighbours(tileLayer.getTileX(pointer.x), tileLayer.getTileY(pointer.y));
+        tileX = tileLayer.getTileX(pointer.x);
+        tileY = tileLayer.getTileY(pointer.y);
+
+        neighbours = Coords.neighbours(tileX, tileY);
+
+        selectedTile = tileData[tileX][tileY];
+
+        
+
+        if(selectedTile === TileStyles.WALL){
+            console.log("it's a wall");
+        }else if(selectedTile === TileStyles.OIL){
+            console.log("it's oil, lets burn");
+        }
+        
     });
 
 }
