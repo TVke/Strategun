a8b53eeaaf83184279449a9ac72984aecd8f00b7
TVke <thomas.verhelst@student.kdg.be>
2017-03-07 11:47:40 +0100

    removed line for vincent

--- js/characters/bomb.js
+++ js/characters/bomb.js
@@ -16,5 +16,4 @@ Bomb.prototype.constructor = Character;
 Bomb.load = function () {
   game.load.image('bomb','assets/grid/mijn.png');
   game.load.image('bombGlow','assets/grid/mijn gloed.png');
-  
 }
