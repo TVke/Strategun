8e5dc8fda1ee2b009b6e2c756093dd3dcf5ab508
Sander Borret <sander.borret@student.kdg.be>
2017-03-07 11:15:29 +0100

    Onzichtbare hover grid toegevoegd

--- js/world/tiles.js
+++ js/world/tiles.js
@@ -1,13 +1,31 @@
-
-
 function Tile(game){
   this.game = game;
 }
 
 Tile.prototype.load = function(){
-  
+	for (let i = 0; i < 25; ++i) {
+		for (let j = 0; j < 50; ++j) {
+			let graphics = game.add.graphics(0, 0);
+			graphics.beginFill(0xFFFFFF);
+			graphics.drawRect(tileSize * j, tileSize * i, tileSize, tileSize);
+			graphics.inputEnabled = true;
+			graphics.events.onInputOver.add(over, this);
+			graphics.events.onInputOut.add(out, this);
+		}
+	}
+}
+
+Tile.prototype.invisibleTiles = function() {
+
 }
 
 Tile.prototype.highlight = function(position, range){
 
+}
+
+function over(item) {
+	item.alpha = 0;
+}
+function out(item) {
+	item.alpha = 1;
 }
