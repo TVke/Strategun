d03e5aa6a848342d3415074c6f53b897e852ee55
TVke <thomas.verhelst@student.kdg.be>
2017-03-08 17:15:01 +0100

    fixed black screen

--- js/game/turns.js
+++ js/game/turns.js
@@ -3,7 +3,7 @@ function Turns() {
 }
 
 
-Menu.prototype.load = function(){
+Turns.prototype.load = function(){
 	this.game.load.image('blueCover', 'assets/grid/blue player.png');
     this.game.load.image('redCover', 'assets/grid/red player.png');
 };
