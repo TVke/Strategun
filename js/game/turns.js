1895d79540a065c03ec90603cbc33bb66d3a2ad5
TVke <thomas.verhelst@student.kdg.be>
2017-03-08 17:16:38 +0100

    fixed errors

--- js/game/turns.js
+++ js/game/turns.js
@@ -4,6 +4,6 @@ function Turns() {
 
 
 Turns.prototype.load = function(){
-	this.game.load.image('blueCover', 'assets/grid/blue player.png');
-    this.game.load.image('redCover', 'assets/grid/red player.png');
+	game.load.image('blueCover', 'assets/grid/blue player.png');
+    game.load.image('redCover', 'assets/grid/red player.png');
 };
