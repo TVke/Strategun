6220181ed7f46232e43506f366cb276604de4743
Sander Borret <sander.borret@student.kdg.be>
2017-03-08 14:27:03 +0100

    Tekst aan navigatie toegevoegd

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
 /// <reference path="../node_modules/phaser/typescript/pixi.d.ts" />
 /// <reference path="../node_modules/phaser/typescript/p2.d.ts" />
 
-
-
-var game = new Phaser.Game(window.innerWidth, window.innerHeight - 66, Phaser.CANVAS, '',
+var game = new Phaser.Game(window.innerWidth, window.innerHeight - document.getElementsByTagName('nav')[0].offsetHeight, Phaser.CANVAS, '',
  {preload:preload,create:create,update:update});
 
 var gameObjects = {
